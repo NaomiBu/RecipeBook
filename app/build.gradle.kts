7df1b42310947ed41a160277392c96383c2036b4
Saima Abdus <saima.abdus@kraken.tech>
2025-09-04 13:33:19 +0100

    Updated Recipe pages and buttons

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.gradle.kotlin.dsl.implementation
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
@@ -40,7 +42,7 @@ android {
 }
 
 dependencies {
-
+    val nav_version = "2.9.3"
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -56,4 +58,6 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    implementation("androidx.navigation:navigation-compose:${nav_version}")
 }
--- app/src/main/java/com/example/recipebook/MainActivity.kt
+++ app/src/main/java/com/example/recipebook/MainActivity.kt
@@ -1,11 +1,10 @@
 package com.example.recipebook
 
-import android.graphics.drawable.Drawable
-import android.media.Image
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -14,12 +13,16 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.foundation.Image
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.platform.LocalContext
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.recipebook.ui.theme.RecipeBookTheme
+import androidx.compose.material3.Button
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,61 +30,151 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             RecipeBookTheme {
+                val navController = rememberNavController()
+
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Homescreen(
+                    NavHost(
+                        navController = navController,
+                        startDestination = "home",
                         modifier = Modifier.padding(innerPadding)
-                    )
-                    Mexican_Burrito(
-                        modifier = Modifier.padding(innerPadding)
-                    )
-
-
+                    ) {
+                        composable("home") {
+                            Homescreen(
+                                onNextScreen = { route ->
+                                    navController.navigate(route)
+                                }
+                            )
+                        }
+                        composable("mexican_burrito") {
+                            Mexican_Burrito(
+                                onNextScreen = { route ->
+                                    navController.navigate(route)
+                                }
+                            )
+                        }
+                        composable("chicken_katsu_curry") {
+                            Chicken_Katsu_Curry(onNextScreen = { route ->
+                                navController.navigate(route)
+                            })
+                        }
+                        composable("waakye") {
+                            Waakye(onNextScreen = { route ->
+                                navController.navigate(route)
+                            })
+                        }
+                        composable("lasagne") {
+                            Lasagne(onNextScreen = { route ->
+                                navController.navigate(route)
+                            })
+                        }
+                        composable("lamb_biryani") {
+                            Lamb_Biryani(onNextScreen = { route ->
+                                navController.navigate(route)
+                            })
+                        }
+                    }
                 }
             }
         }
     }
 }
 
-@Composable
-fun Homescreen ( modifier: Modifier = Modifier) {
-    Column () {
-        Text(
-            text = stringResource(R.string.homepage_title),
-            modifier = modifier
-        )
-        LazyColumn {
-            // Add a single item
-            item {
-                Text(text = stringResource(R.string.recipe_one))
-            }
-            item {
-                Text(text = stringResource(R.string.recipe_two))
-            }
-            item {
-                Text(text = stringResource(R.string.recipe_three))
-            }
-            item {
-                Text(text = stringResource(R.string.recipe_four))
-            }
-            item {
-                Text(text = stringResource(R.string.recipe_five))
-            }
+
+@Composable()
+fun App() {
+    // We won't use this `navController` for now,
+    // but it's needed to setup the `NavHost`.
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = "home")
+     {
+
+        // Here we declare all screens,
+        // giving them a "route", which is a unique name
+        // for each one.
+
+        composable(route = "home") {
+            Homescreen(onNextScreen = {
+                navController.navigate("details")
+            })
+        }
+
+        composable(route = "details") {
+            Mexican_Burrito(onNextScreen = {
+                navController.navigate("details")
+            })
+        }
+
+        composable(route = "details") {
+            Chicken_Katsu_Curry(onNextScreen = {
+                navController.navigate("details")
+            })
+        }
+
+        composable(route = "details") {
+            Waakye(onNextScreen = {
+                navController.navigate("details")
+            })
+        }
+
+        composable(route = "details") {
+            Lasagne(onNextScreen = {
+                navController.navigate("details")
+            })
+        }
+
+        composable(route = "details") {
+            Lamb_Biryani(onNextScreen = {
+                navController.navigate("details")
+            })
         }
     }
 }
 
 @Composable
-fun Mexican_Burrito (modifier: Modifier = Modifier) {
+fun Homescreen (onNextScreen: (String) -> Unit, modifier: Modifier = Modifier) {
+    val context = LocalContext.current
+    val recipes = context.resources.getStringArray(R.array.recipes)
+    Column () {
+        Text(
+            text = stringResource(R.string.homepage_title),
+            modifier = modifier
+        )
+        Button(onClick = { onNextScreen("mexican_burrito") }) {
+            Text(text = recipes[0])
+        }
+        Button(onClick = { onNextScreen("chicken_katsu_curry") }) {
+            Text(text = recipes[1])
+        }
+        Button(onClick = { onNextScreen("waakye") }) {
+            Text(text = recipes[2])
+        }
+        Button(onClick = { onNextScreen("lasagne") }) {
+            Text(text = recipes[3])
+        }
+        Button(onClick = { onNextScreen("lamb_biryani") }) {
+            Text(text = recipes[4])
+        }
+    }
+}
+
+@Composable
+fun Mexican_Burrito (onNextScreen: (String) -> Unit, modifier: Modifier = Modifier) {
+    val context = LocalContext.current
+    val recipes = context.resources.getStringArray(R.array.recipes)
  Column (){
+     Button(onClick = { onNextScreen("home") }) {
+         Text(text = "home")
+     }
      val context = LocalContext.current
      val ingredients = context.resources.getStringArray(R.array.burrito_ingredients)
      val steps = context.resources.getStringArray(R.array.burrito_steps)
     Text(
-        text =  stringResource(R.string.recipe_one)
+        text =  recipes[0]
     )
      Image(
         painter = painterResource(R.drawable.burrito),
-         contentDescription = "Image of burrito"
+         contentDescription = "Image of Burrito"
      )
      Text(
          text = stringResource(R.string.burrito_description)
@@ -104,10 +197,168 @@ fun Mexican_Burrito (modifier: Modifier = Modifier) {
  }
 }
 
+@Composable
+fun Chicken_Katsu_Curry (onNextScreen: (String) -> Unit, modifier: Modifier = Modifier) {
+    val context = LocalContext.current
+    val recipes = context.resources.getStringArray(R.array.recipes)
+    Column (){
+        Button(onClick = { onNextScreen("home") }) {
+            Text(text = "home")
+        }
+        val context = LocalContext.current
+        val ingredients = context.resources.getStringArray(R.array.chicken_katsu_curry_ingredients)
+        val steps = context.resources.getStringArray(R.array.chicken_katsu_curry_steps)
+        Text(
+            text =  recipes[1]
+        )
+        Image(
+            painter = painterResource(R.drawable.chicken_katsu),
+            contentDescription = "Image of Chicken Katsu Curry"
+        )
+        Text(
+            text = stringResource(R.string.chicken_katsu_curry_description)
+        )
+        LazyColumn {
+            // Add a single item
+            ingredients.forEach {
+                    ingredient -> item { Text(text = ingredient)}
+            }
+
+        }
+        LazyColumn {
+            // Add a single item
+            steps.forEach {
+                    step -> item { Text(text = step)}
+            }
+
+        }
+
+    }
+}
+
+
+@Composable
+fun Waakye (onNextScreen: (String) -> Unit, modifier: Modifier = Modifier) {
+    val context = LocalContext.current
+    val recipes = context.resources.getStringArray(R.array.recipes)
+    Column (){
+        Button(onClick = { onNextScreen("home") }) {
+            Text(text = "home")
+        }
+        val context = LocalContext.current
+        val ingredients = context.resources.getStringArray(R.array.waakye_ingredients)
+        val steps = context.resources.getStringArray(R.array.waakye_steps)
+        Text(
+            text =  recipes[2]
+        )
+        Image(
+            painter = painterResource(R.drawable.waakye),
+            contentDescription = "Image of Waakye"
+        )
+        Text(
+            text = stringResource(R.string.waakye_description)
+        )
+        LazyColumn {
+            // Add a single item
+            ingredients.forEach {
+                    ingredient -> item { Text(text = ingredient)}
+            }
+
+        }
+        LazyColumn {
+            // Add a single item
+            steps.forEach {
+                    step -> item { Text(text = step)}
+            }
+
+        }
+
+    }
+}
+
+@Composable
+fun Lasagne (onNextScreen: (String) -> Unit, modifier: Modifier = Modifier) {
+    val context = LocalContext.current
+    val recipes = context.resources.getStringArray(R.array.recipes)
+    Column (){
+        Button(onClick = { onNextScreen("home") }) {
+            Text(text = "home")
+        }
+        val context = LocalContext.current
+        val ingredients = context.resources.getStringArray(R.array.lasagne_ingredients)
+        val steps = context.resources.getStringArray(R.array.lasagne_steps)
+        Text(
+            text =  recipes[3]
+        )
+        Image(
+            painter = painterResource(R.drawable.lasagne),
+            contentDescription = "Image of Lasagne"
+        )
+        Text(
+            text = stringResource(R.string.lasagne_description)
+        )
+        LazyColumn {
+            // Add a single item
+            ingredients.forEach {
+                    ingredient -> item { Text(text = ingredient)}
+            }
+
+        }
+        LazyColumn {
+            // Add a single item
+            steps.forEach {
+                    step -> item { Text(text = step)}
+            }
+
+        }
+
+    }
+}
+
+@Composable
+fun Lamb_Biryani (onNextScreen: (String) -> Unit, modifier: Modifier = Modifier) {
+    val context = LocalContext.current
+    val recipes = context.resources.getStringArray(R.array.recipes)
+    Column (){
+        Button(onClick = { onNextScreen("home") }) {
+            Text(text = "home")
+        }
+        val context = LocalContext.current
+        val ingredients = context.resources.getStringArray(R.array.lamb_biryani_ingredients)
+        val steps = context.resources.getStringArray(R.array.lamb_biryani_steps)
+        Text(
+            text =  recipes[4]
+        )
+        Image(
+            painter = painterResource(R.drawable.lamb_biryani),
+            contentDescription = "Image of Lasagne"
+        )
+        Text(
+            text = stringResource(R.string.lamb_biryani_description)
+        )
+        LazyColumn {
+            // Add a single item
+            ingredients.forEach {
+                    ingredient -> item { Text(text = ingredient)}
+            }
+
+        }
+        LazyColumn {
+            // Add a single item
+            steps.forEach {
+                    step -> item { Text(text = step)}
+            }
+        }
+    }
+}
+
+
 @Preview(showBackground = true)
 @Composable
 fun RecipeBookPreview() {
     RecipeBookTheme {
-        Homescreen()
+        Homescreen(
+            onNextScreen = {}
+        )
     }
 }
