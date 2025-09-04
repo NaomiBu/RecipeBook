7f566b3b9d8ff535515060974bb19629080cd7e6
Naomi <naomibuadee1@gmail.com>
2025-09-04 16:25:14 +0100

    Added styling to recipe pages

--- app/src/main/java/com/example/recipebook/MainActivity.kt
+++ app/src/main/java/com/example/recipebook/MainActivity.kt
@@ -5,10 +5,16 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -21,7 +27,20 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.recipebook.ui.theme.RecipeBookTheme
+import androidx.compose.material3.ColorScheme
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 
 
 class MainActivity : ComponentActivity() {
@@ -135,26 +154,72 @@ fun App() {
 fun Homescreen (onNextScreen: (String) -> Unit, modifier: Modifier = Modifier) {
     val context = LocalContext.current
     val recipes = context.resources.getStringArray(R.array.recipes)
-    Column () {
+    Column ( modifier = Modifier
+        .fillMaxSize(),
+
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         Text(
             text = stringResource(R.string.homepage_title),
-            modifier = modifier
+            fontSize = 30.sp,
+            fontFamily = FontFamily.Cursive,
+            modifier = Modifier
+                .padding(bottom = 40.dp, top = 20.dp)
         )
-        Button(onClick = { onNextScreen("mexican_burrito") }) {
+        Button( modifier = Modifier
+                    .padding(bottom = 18.dp)
+                     , colors = ButtonDefaults.buttonColors(
+                         containerColor = colorResource(R.color.maximum_green),     // Background
+                    contentColor = Color.White),
+
+
+                        onClick = { onNextScreen("mexican_burrito") }) {
             Text(text = recipes[0])
         }
-        Button(onClick = { onNextScreen("chicken_katsu_curry") }) {
+        Button(modifier = Modifier
+                 .padding(bottom = 18.dp),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = colorResource(R.color.maximum_green),     // Background
+                         contentColor = Color.White),
+
+            onClick = { onNextScreen("chicken_katsu_curry") }) {
             Text(text = recipes[1])
         }
-        Button(onClick = { onNextScreen("waakye") }) {
+        Button( modifier = Modifier
+                 .padding(bottom = 18.dp),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = colorResource(R.color.maximum_green),     // Background
+                            contentColor = Color.White),
+
+            onClick = { onNextScreen("waakye") }) {
             Text(text = recipes[2])
         }
-        Button(onClick = { onNextScreen("lasagne") }) {
-            Text(text = recipes[3])
+        Button( modifier = Modifier
+                .padding(bottom = 18.dp),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = colorResource(R.color.maximum_green),
+                            contentColor = Color.White),
+                    onClick = { onNextScreen("lasagne") }) {
+                         Text(text = recipes[3])
         }
-        Button(onClick = { onNextScreen("lamb_biryani") }) {
-            Text(text = recipes[4])
+        Button( modifier = Modifier
+                .padding(bottom = 18.dp), colors = ButtonDefaults.buttonColors(
+                    containerColor = colorResource(R.color.maximum_green),
+                        contentColor = Color.White),
+                    onClick = { onNextScreen("lamb_biryani") }) {
+                        Text(text = recipes[4])
         }
+        Image(
+            painter = painterResource(R.drawable.collage),
+            contentDescription = "Image of Burrito",
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f)
+                .padding(top=100.dp)
+            ,
+            contentScale = ContentScale.Crop // scales nicely
+        )
     }
 }
 
@@ -162,37 +227,106 @@ fun Homescreen (onNextScreen: (String) -> Unit, modifier: Modifier = Modifier) {
 fun Mexican_Burrito (onNextScreen: (String) -> Unit, modifier: Modifier = Modifier) {
     val context = LocalContext.current
     val recipes = context.resources.getStringArray(R.array.recipes)
- Column (){
-     Button(onClick = { onNextScreen("home") }) {
-         Text(text = "home")
-     }
-     val context = LocalContext.current
-     val ingredients = context.resources.getStringArray(R.array.burrito_ingredients)
-     val steps = context.resources.getStringArray(R.array.burrito_steps)
-    Text(
-        text =  recipes[0]
-    )
-     Image(
-        painter = painterResource(R.drawable.burrito),
-         contentDescription = "Image of Burrito"
+    val ingredients = context.resources.getStringArray(R.array.burrito_ingredients)
+    val steps = context.resources.getStringArray(R.array.burrito_steps)
+    val scrollState = rememberScrollState()
+
+// Column (modifier = Modifier
+//     .verticalScroll(scrollState)
+//     .fillMaxSize()
+// )
+    Column (modifier = Modifier
+        .fillMaxSize()
+        .verticalScroll(scrollState),
+        horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+     Row (modifier = Modifier
      )
-     Text(
-         text = stringResource(R.string.burrito_description)
+        {Text(
+            text = stringResource(R.string.homepage_title),
+                fontSize = 30.sp,
+                    fontFamily = FontFamily.Cursive,
+         modifier = Modifier
+             .padding(bottom = 40.dp, top = 20.dp, end = 50.dp)
      )
-     LazyColumn {
-         // Add a single item
-         ingredients.forEach {
-             ingredient -> item { Text(text = ingredient)}
+         Button( modifier = Modifier
+             .padding(10.dp),
+                colors = ButtonDefaults.buttonColors(
+                containerColor = colorResource(R.color.maximum_green),     // Background
+                contentColor = Color.White),
+                 onClick = { onNextScreen("home") }) {
+            Icon( modifier = Modifier
+                .size(18.dp),
+                painter =  painterResource(R.drawable.home),
+                contentDescription = "Home",
+
+             )
+
+         }}
+
+
+     Column (modifier = Modifier
+         .fillMaxWidth(),
+          horizontalAlignment = Alignment.CenterHorizontally)
+     {
+         Text(
+             text =  recipes[0],
+             modifier = Modifier
+                 .padding(bottom = 18.dp),
+             fontWeight = FontWeight.Bold,
+             fontSize = 20.sp
+
+         )
+         Image(
+             painter = painterResource(R.drawable.burrito),
+             contentDescription = "Image of Burrito",
+             modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+
+         )
+         Text(
+                 text = stringResource(R.string.burrito_description),
+         modifier = Modifier
+             .fillMaxWidth()
+             .padding(top = 18.dp, bottom = 18.dp, start = 20.dp, end = 20.dp),
+             textAlign = TextAlign.Left
+         )
+         Text(
+             modifier = Modifier
+                 .fillMaxWidth()
+                 .padding(top = 18.dp, bottom = 2.dp, start = 20.dp, end = 20.dp),
+             text = stringResource(R.string.ingredients)
+         )
+         Column (modifier = Modifier
+             .fillMaxWidth()
+             .padding(top = 5.dp, bottom = 10.dp, start = 20.dp, end = 20.dp),
+         ) {
+             // Add a single item
+             ingredients.forEach {
+                     ingredient ->  Text(text = "• $ingredient")
+             }
+
+         }
+         Text(
+             modifier = Modifier
+                 .fillMaxWidth()
+                 .padding(top = 18.dp, bottom = 2.dp, start = 20.dp, end = 20.dp),
+             text = stringResource(R.string.steps)
+         )
+         Column (modifier = Modifier
+             .fillMaxWidth()
+             .padding(top = 5.dp, bottom = 10.dp, start = 20.dp, end = 20.dp),
+         ){
+             // Add a single item
+             steps.forEach {
+                     step -> Text(text = "• $step")
+             }
+
          }
 
      }
-     LazyColumn {
-         // Add a single item
-         steps.forEach {
-                 step -> item { Text(text = step)}
-         }
 
-     }
 
  }
 }
@@ -358,6 +492,56 @@ fun Lamb_Biryani (onNextScreen: (String) -> Unit, modifier: Modifier = Modifier)
 fun RecipeBookPreview() {
     RecipeBookTheme {
         Homescreen(
+            onNextScreen = {}
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MexicanBurritoPreview() {
+    RecipeBookTheme {
+        Mexican_Burrito(
+            onNextScreen = {}
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ChickenKatsuCurryPreview() {
+    RecipeBookTheme {
+        Chicken_Katsu_Curry(
+            onNextScreen = {}
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun WaakyePreview() {
+    RecipeBookTheme {
+        Waakye(
+            onNextScreen = {}
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LasagnePreview() {
+    RecipeBookTheme {
+        Lasagne(
+            onNextScreen = {}
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LambBiryaniPreview() {
+    RecipeBookTheme {
+        Lamb_Biryani(
             onNextScreen = {}
         )
     }
